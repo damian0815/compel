c2913351b5e503e8c472d97b8c853edd501f0896
Damian Stewart <d@damianstewart.com>
2023-02-19 21:29:32 +0100

    add batched input docs

--- compel-demo.py
+++ compel-demo.py
@@ -9,15 +9,12 @@ pipeline = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1
 pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config,
                                                              algorithm_type="dpmsolver++")
 
+prompts = ["a cat playing with a ball++ in the forest", "a cat playing with a ball in the forest"]
+
 compel = Compel(tokenizer=pipeline.tokenizer, text_encoder=pipeline.text_encoder)
+prompt_embeds = torch.cat([compel.build_conditioning_tensor(prompt) for prompt in prompts])
+images = pipeline(prompt_embeds=prompt_embeds, num_inference_steps=10, width=256, height=256).images
+print(images)
 
-prompt = "a cat playing with a ball in the forest"
-seed = 123
-
-embeds = compel.build_conditioning_tensor(prompt)
-fixed_seed_generator = Generator(device="cpu" if compel.device.type == "mps" else compel.device).manual_seed(seed)
-image = pipeline(prompt_embeds=embeds,
-                 num_inference_steps=7,
-                 generator=fixed_seed_generator).images[0]
-
-image.save('cat-ball-forest.png')
+images[0].save('/tmp/img0.jpg')
+images[1].save('/tmp/img1.jpg')
