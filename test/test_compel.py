ddb738d734c39ef22712f24f71abe01b3fbaace3
Damian Stewart <d@damianstewart.com>
2025-05-17 09:10:20 +0200

    fix test

--- test/test_compel.py
+++ test/test_compel.py
@@ -306,11 +306,11 @@ class CompelTestCase(unittest.TestCase):
 
     def test_end_to_end_split(self):
 
-        max_length = 77
+        max_length = 5
         tokenizer = DummyTokenizer(model_max_length=max_length)
         text_encoder = DummyTransformer()
         compel = Compel(tokenizer=tokenizer, text_encoder=text_encoder, truncate_long_prompts=False)
-        text = "As he crossed toward the pharmacy at the corner he involuntarily turned his head because of a burst of light that had ricochetedfromhistemple,andsaw,withthatquicksmilewithwhichwegreetarainboworarose,ablindingly white parallelogram of sky being unloaded from the van—a dresserwithmirrorsacross whichasacrossacinema screen, passed a flawlessly clear reflection of boughs sliding and swaying not arboreally, but with a human vacillation, produced by the nature of those who were carrying this sky, these boughs, this gliding façade. As he crossed toward the pharmacy at the corner he involuntarily turned his head because of a burst of light that had ricochetedfromhistemple,andsaw,withthatquicksmilewithwhichwegreetarainboworarose,ablindingly white parallelogram of sky being unloaded from the van—a dresserwithmirrorsacross whichasacrossacinema screen, passed a flawlessly clear reflection of boughs sliding and swaying not arboreally, but with a human vacillation, produced by the nature of those who were carrying this sky, these boughs, this gliding façade."
+        text = "a b gone</w> a b .</w> gone</w> home</w> a .</w>"
         # should not raise any exceptions
         _ = compel(text)
 
